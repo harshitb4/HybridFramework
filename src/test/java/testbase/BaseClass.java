ce9800dce9f1c117be5edaa5b79309c68bc60982
Harshit <harshitb394@gmail.com>
2025-03-06 22:46:16 +0530

    Added log4j2 file

--- src/test/java/testbase/BaseClass.java
+++ src/test/java/testbase/BaseClass.java
@@ -1,5 +1,7 @@
 package testbase;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -8,19 +10,26 @@ import org.testng.annotations.BeforeClass;
 public class BaseClass {
 
 	public WebDriver driver;
+	public Logger logger;
 	
 	@BeforeClass
 	public void login() throws InterruptedException
 	{
+		logger = LogManager.getLogger(this.getClass());
+		
 		driver = new ChromeDriver();
 		driver.get("https://tutorialsninja.com/demo/");
 		Thread.sleep(10000);
 		driver.manage().window().maximize();
+		
+		logger.info("Application is Launched");
 	}
 	
 	@AfterClass
 	public void tearDown()
 	{
 		driver.quit();
+		
+		logger.info("Driver quit Statement");
 	}
 }
