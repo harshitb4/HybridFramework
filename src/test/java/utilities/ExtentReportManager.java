08cb899d42b15a251d82cb1ab34132db41c6afe8
Harshit <harshitb394@gmail.com>
2025-03-08 23:15:57 +0530

    Added ExtentReportManager class

---
+++ src/test/java/utilities/ExtentReportManager.java
@@ -0,0 +1,27 @@
+package utilities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportManager implements ITestListener {
+
+	public ExtentSparkReporter sparkReporter;
+	public ExtentReports extent;
+	public ExtentTest test;
+	String repName;
+	
+	@Override
+	public void onStart(ITestContext context)
+	{
+		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+		repName = "Test-Report-"+timeStamp+".html";
+		sparkReporter = new ExtentSparkReporter(".\\reports\\"+repName);
+	}
+}
