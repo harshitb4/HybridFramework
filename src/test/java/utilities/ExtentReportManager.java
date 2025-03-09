4e01e861fd8af607fad428c7b3493ec17803705f
Harshit <harshitb394@gmail.com>
2025-03-09 19:20:19 +0530

    Updated ExtentReportManager class

--- src/test/java/utilities/ExtentReportManager.java
+++ src/test/java/utilities/ExtentReportManager.java
@@ -2,6 +2,7 @@ package utilities;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import org.testng.ITestContext;
 import org.testng.ITestListener;
@@ -9,6 +10,7 @@ import org.testng.ITestListener;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentReportManager implements ITestListener {
 
@@ -18,10 +20,34 @@ public class ExtentReportManager implements ITestListener {
 	String repName;
 	
 	@Override
-	public void onStart(ITestContext context)
+	public void onStart(ITestContext testContext)
 	{
 		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
 		repName = "Test-Report-"+timeStamp+".html";
 		sparkReporter = new ExtentSparkReporter(".\\reports\\"+repName);
+		
+		sparkReporter.config().setDocumentTitle("OpenCart Automation Report"); //Title of the Report
+		sparkReporter.config().setReportName("OpenCart Functional Testing"); //Name of the Report
+		sparkReporter.config().setTheme(Theme.DARK);
+		
+		extent = new ExtentReports();
+		extent.attachReporter(sparkReporter);
+		extent.setSystemInfo("Application", "OpenCart");
+		extent.setSystemInfo("Module", "Admin");
+		extent.setSystemInfo("Sub Module", "Customers");
+		extent.setSystemInfo("User Name", System.getProperty("user.name"));
+		extent.setSystemInfo("Environment", "QA");
+		
+		String os = testContext.getCurrentXmlTest().getParameter("os");
+		extent.setSystemInfo("OperatingSystem", os);
+		
+		String browser = testContext.getCurrentXmlTest().getParameter("browser");
+		extent.setSystemInfo("Browser", browser);
+		
+		List<String> includedGroups = testContext.getCurrentXmlTest().getIncludedGroups();
+		if(!includedGroups.isEmpty())
+		{
+			extent.setSystemInfo("Groups", includedGroups.toString());
+		}
 	}
 }
