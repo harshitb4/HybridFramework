e722ec1a6c4447699e6ba3cda300bf6f7b615016
Harshit <harshitb394@gmail.com>
2025-03-10 22:18:09 +0530

    Updated ExtentReportManager class

--- src/test/java/testcases/TC002_AccountRegistrationTest.java
+++ src/test/java/testcases/TC002_AccountRegistrationTest.java
@@ -36,4 +36,16 @@ public class TC002_AccountRegistrationTest extends BaseClass {
 			Assert.assertTrue(false);
 		}
 	}
+	
+	@Test
+	public void a()
+	{
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void b()
+	{
+		Assert.assertTrue(true);
+	}
 }
--- src/test/java/utilities/ExtentReportManager.java
+++ src/test/java/utilities/ExtentReportManager.java
@@ -1,14 +1,19 @@
 package utilities;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
 import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestResult;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
@@ -22,8 +27,10 @@ public class ExtentReportManager implements ITestListener {
 	@Override
 	public void onStart(ITestContext testContext)
 	{
+		System.out.println("OnStart started");
 		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
 		repName = "Test-Report-"+timeStamp+".html";
+		System.out.println(repName);
 		sparkReporter = new ExtentSparkReporter(".\\reports\\"+repName);
 		
 		sparkReporter.config().setDocumentTitle("OpenCart Automation Report"); //Title of the Report
@@ -48,6 +55,46 @@ public class ExtentReportManager implements ITestListener {
 		if(!includedGroups.isEmpty())
 		{
 			extent.setSystemInfo("Groups", includedGroups.toString());
+		}
+	}
+	
+	public void onTestSuccess(ITestResult result)
+	{
+		if (test == null) {  // Create class-level entry only once
+	        test = extent.createTest(result.getTestClass().getName());
+	    }
+	    test.createNode(result.getName()).log(Status.PASS, result.getName() + " got successfully executed");
+		//test = extent.createTest(result.getTestClass().getName());
+		//test.assignCategory(result.getMethod().getGroups());
+		//test.log(Status.PASS, result.getName()+"got successfully executed");
+	}
+	
+	public void onTestFailure(ITestResult result)
+	{	
+		test = extent.createTest(result.getTestClass().getName());
+		test.assignCategory(result.getMethod().getGroups());
+		test.log(Status.FAIL, result.getName()+"got failed");
+	}
+	
+	public void onTestSkipped(ITestResult result)
+	{
+		test = extent.createTest(result.getTestClass().getName());
+		test.assignCategory(result.getMethod().getGroups());
+		test.log(Status.SKIP, result.getName()+"got skipped");
+	}
+	
+	public void onFinish(ITestContext testContext)
+	{
+		extent.flush();
+		System.out.println("Flushed Successfully");
+		String pathOfExtentReport = System.getProperty("user.dir")+"\\reports\\"+repName;
+		File extentReport = new File(pathOfExtentReport);
+		
+		try {
+			Desktop.getDesktop().browse(extentReport.toURI());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
