d03cff461ef4fe7f3245318883e9079787ae7e50
Harshit <harshitb394@gmail.com>
2025-03-18 23:03:34 +0530

    Updated BaseClass

--- src/test/java/testbase/BaseClass.java
+++ src/test/java/testbase/BaseClass.java
@@ -35,14 +35,17 @@ public class BaseClass {
 	@Parameters({"os","browser"})
 	public void login(String os,String browser) throws InterruptedException, IOException
 	{
+		logger = LogManager.getLogger(this.getClass());
+		
+		logger.info("Loading configuration properties");
 		FileReader file = new FileReader("./src/test//resources//config.properties");
 		p= new Properties();
 		p.load(file);
 		
-		logger = LogManager.getLogger(this.getClass());
-		
+		logger.info("Execution environment: "+p.getProperty("execution_env"));
 		if(p.getProperty("execution_env").equalsIgnoreCase("remote"))
 		{
+			logger.info("Setting up remote execution...");
 			DesiredCapabilities capabilities = new DesiredCapabilities();
 			
 			if(os.equalsIgnoreCase("windows"))
@@ -59,7 +62,7 @@ public class BaseClass {
 			}
 			else
 			{
-				System.out.println("Invalid Platform");
+				logger.error("Invalid platform "+os);
 				return;
 			}
 			
@@ -68,74 +71,91 @@ public class BaseClass {
 			  case "chrome":capabilities.setBrowserName("chrome"); break;
 			  case "edge":capabilities.setBrowserName("MicrosoftEdge"); break;
 			  case "firefox":capabilities.setBrowserName("firefox"); break;
-			  default : System.out.println("No browser Matching"); return;
+			  default : logger.error("Invalid browser name: "+browser); return;
 			}
 			
 			@SuppressWarnings("deprecation")
 			URL url = new URL("http://localhost:4444/wd/hub");
 			driver = new RemoteWebDriver(url, capabilities);
-			
+			logger.info("Remote WebDriver initialized with "+browser+" on "+os);
 		}
 		
 		else if(p.getProperty("execution_env").equalsIgnoreCase("local"))
 		{
+			logger.info("Setting up local execution...");
 			switch(browser.toLowerCase())
 			{
 			case "chrome":driver = new ChromeDriver(); break;
 			case "edge":driver = new EdgeDriver(); break;
 			case "firefox":driver = new FirefoxDriver(); break;
-			default:System.out.println("Wrong browser name"); return;
+			default:logger.error("Invalid browser name: "+browser); return;
 			}
+			logger.info("Browser launched successfully");
 		}
 		
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
+		logger.info("Launching application: "+p.getProperty("appURL"));
 		driver.get(p.getProperty("appURL"));
 		Thread.sleep(5000);
 		driver.manage().window().maximize();
 		Thread.sleep(3000);
 		
-		logger.info("Application is Launched");
+		logger.info("Application Launched successfully");
 	}
 	
 	@AfterClass
 	public void tearDown()
 	{
-		driver.quit();
-		
-		logger.info("Driver quit Statement");
+		if(driver!=null)
+		{
+			driver.quit();
+			logger.info("Driver quit successfully");
+		}
+		else
+		{
+			logger.warn("Driver was already null before quit");
+		}
 	}
 	
 	public String randomString()
 	{
 		String generatedString = RandomStringUtils.randomAlphabetic(5);
+		logger.debug("Generated randon string: "+generatedString);
 		return generatedString;
 	}
 	
 	public String randomNumber()
 	{
 		String generatedNumber = RandomStringUtils.randomNumeric(10);
+		logger.debug("Generated random number: "+generatedNumber);
 		return generatedNumber;
 	}
 	
 	public String randomAlphaNumeric()
 	{
 		String randomAlphaNumeric = RandomStringUtils.randomAlphanumeric(6);
+		logger.debug("Generated alphanumeric: "+randomAlphaNumeric);
 		return randomAlphaNumeric;
 	}
 	
 	public String captureScreen(String tName)
 	{
 		String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-		
-		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
-		File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
-		
 		String targetFilePath = System.getProperty("user.dir")+"\\screenshots\\"+tName+"_"+timeStamp+".png";
-		File targetFile = new File(targetFilePath);
 		
-		sourceFile.renameTo(targetFile);
+		try {
+		   TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+		   File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+		   File targetFile = new File(targetFilePath);
+		   sourceFile.renameTo(targetFile);
+		   
+		   logger.info("Screenshot captured: "+targetFilePath);
+		}catch(Exception e)
+		{
+			logger.error("Failed to capture screenshot for "+tName, e);
+		}
 		
 		return targetFilePath;
 	}
