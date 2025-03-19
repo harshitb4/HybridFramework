18e25eb0a8eaa12714d266787a627785f5df76ca
Harshit <harshitb394@gmail.com>
2025-03-19 22:36:32 +0530

    Updated loggers in TC001_LoginTestDDT class

--- src/test/java/testcases/TC001_LoginTestDDT.java
+++ src/test/java/testcases/TC001_LoginTestDDT.java
@@ -14,14 +14,21 @@ public class TC001_LoginTestDDT extends BaseClass {
 	@Test(dataProvider="LoginData", dataProviderClass=DataProviders.class,groups= {"dataDriven"} )
 	public void verify_Login(String email, String password, String result) throws InterruptedException
 	{
+		logger.info("Starting Login Test with email: "+email+" and expected result: "+result);
+		
 		HomePage hp = new HomePage(driver);
 		hp.clickMyAccount();
+		logger.info("Clicked on MyAccount");
 		hp.clickLogin();
+		logger.info("Navigated to Login Page");
 		
 		LoginPage lp = new LoginPage(driver);
 		lp.setEmail(email);
+		logger.info("Entered email: "+email);
 		lp.setPassword(password);
+		logger.info("Entered password");
 		lp.clickLogin();
+		logger.info("Clicked on Login button");
 		
 		MyAccountPage my_Acc = new MyAccountPage(driver);
 		boolean accPage = my_Acc.isMyAccountPageExist();
@@ -30,11 +37,14 @@ public class TC001_LoginTestDDT extends BaseClass {
 		{
 			if(accPage)
 			{
+				logger.info("Login Successful. User is on My Account page");
 				Assert.assertTrue(true);
 				my_Acc.clickLinkLogout();
+				logger.info("Logged out successfully");
 			}
 			else
 			{
+				logger.info("Login failed for valid credentials.");
 				Assert.assertTrue(false);
 			}
 		}
@@ -42,14 +52,17 @@ public class TC001_LoginTestDDT extends BaseClass {
 		{
 			if(accPage)
 			{
+				logger.info("Login successful for invalid credentials. Test failed");
 				Assert.assertTrue(false);
 				my_Acc.clickLinkLogout();
+				logger.info("Logged out successfully.");
 			}
 			else
 			{
+				logger.info("Login failed as expected for invalid credentials");
 				Assert.assertTrue(true);
 			}
 		}
-		
+		logger.info("Test execution completed for email: "+email);
 	}
 }
